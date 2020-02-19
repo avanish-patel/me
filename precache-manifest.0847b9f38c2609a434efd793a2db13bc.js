892006f6da25fcb9aeeabe577028e8eacc0808d4
Avanish patel <avanishpatel@Avanishs-MacBook-Pro.local>
2020-02-18 20:55:17 -0600

    Updates

---
+++ precache-manifest.0847b9f38c2609a434efd793a2db13bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bd7524005a6792d38d2",
+    "url": "/me/static/css/main.021b1f47.chunk.css"
+  },
+  {
+    "revision": "1bd7524005a6792d38d2",
+    "url": "/me/static/js/main.1bd75240.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "3aa8c0f78faaf10b550458b3e1eeebdb",
+    "url": "/me/index.html"
+  }
+];
